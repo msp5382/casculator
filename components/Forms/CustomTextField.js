cd44977c2c5000d640c145753e6dc99781fb9693
peammy1146 <peammy2010@hotmail.com>
2021-07-25 09:19:59 +0700

    input responsive

--- components/Forms/CustomTextField.js
+++ components/Forms/CustomTextField.js
@@ -1,7 +1,7 @@
 const InputField = ({ label, ...props }) => {
     return (
-        <div className='relative border-2 rounded-2xl m-2 focus-within:border-base-bg'>
-            <input type='text' placeholder=" " name={label} className='block p-2 text-lg w-36 appearance-none focus:outline-none bg-transparent' {...props} />
+        <div className='relative border-2 rounded-2xl mr-2 ml-2 focus-within:border-base-bg'>
+            <input placeholder=" " name={label} className='block p-2 text-lg w-full appearance-none focus:outline-none bg-transparent' {...props} />
             <label label={label} className='absolute bg-thin-white text-gray-400 p-2 top-0 -z-1 origin-0 duration-300'>{label}</label>
         </div>
     )
