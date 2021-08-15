ead63126bfa77ca34d1a1673cc56c8ecace300ea
peammy1146 <peammy2010@hotmail.com>
2021-08-16 00:17:26 +0700

    og head meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from 'next/router'
+import Head from "next/head"
 // Components
 import ProgressBar from "../components/Bars/TailProg";
 import ProgressBarH from "../components/Bars/TailProgH";
@@ -22,48 +23,69 @@ const Index = () => {
   }, [page])
 
   return (
-    <div ref={mainRef} className="w-screen h-auto p-0 m-0">
-      <div className="w-full fixed top-0 z-20">
-        <Nav />
-      </div>
-      <div className="w-full h-screen flex flex-col justify-between items-center overflow-hidden">
-        <div className="mt-24 hidden lg:flex">
-          <ProgressBar />
+    <>
+      <Head>
+        <meta
+          property="og:url"
+          content="https://web-khun-peam.vercel.app"
+        />
+        <meta
+          property="og:title"
+          content="Casculator"
+        />
+        <meta
+          property="og:description"
+          content="เว็บไซต์สำหรับน้องๆต้องการคิดคะแนนในระบบ TCAS รอบที่ 3 และระบบทดลองจัดอันดับมหาลัย"
+        />
+        <meta
+          property="og:image"
+          content="/og_image.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+      </Head>
+      <div ref={mainRef} className="w-screen h-auto p-0 m-0">
+        <div className="w-full fixed top-0 z-20">
+          <Nav />
         </div>
-        <div className="mt-16 h-[40px] flex lg:hidden" />
-        <div className="flex flex-col items-center flex-center">
-          <CountDown />
-        </div>
-        <div className="flex flex-col items-center">
-          <Link href="/view_faculties">
-            <a>
-              <div className="py-4 px-5 text-center text-2xl rounded-3xl text-thin-white bg-base">
-                <h1>
-                  ดูสถิติคณะ
-                </h1>
-              </div>
-            </a>
-          </Link>
-          <div onClick={() => {
-            if (mainRef.current != undefined) {
-              if (mainRef.current.offsetWidth > 1024) {
-                donateRef.current?.scrollIntoView({ behavior: "smooth" })
-              } else {
-                proRef.current?.scrollIntoView({ behavior: "smooth" })
+        <div className="w-full h-screen flex flex-col justify-between items-center overflow-hidden">
+          <div className="mt-24 hidden lg:flex">
+            <ProgressBar />
+          </div>
+          <div className="mt-16 h-[40px] flex lg:hidden" />
+          <div className="flex flex-col items-center flex-center">
+            <CountDown />
+          </div>
+          <div className="flex flex-col items-center">
+            <Link href="/view_faculties">
+              <a>
+                <div className="py-4 px-5 text-center text-2xl rounded-3xl text-thin-white bg-base">
+                  <h1>
+                    ดูสถิติคณะ
+                  </h1>
+                </div>
+              </a>
+            </Link>
+            <div onClick={() => {
+              if (mainRef.current != undefined) {
+                if (mainRef.current.offsetWidth > 1024) {
+                  donateRef.current?.scrollIntoView({ behavior: "smooth" })
+                } else {
+                  proRef.current?.scrollIntoView({ behavior: "smooth" })
+                }
               }
-            }
-          }} className="my-5 animate-bounce cursor-pointer">
-            <ion-icon size="large" name="chevron-down-outline"></ion-icon>
+            }} className="my-5 animate-bounce cursor-pointer">
+              <ion-icon size="large" name="chevron-down-outline"></ion-icon>
+            </div>
           </div>
         </div>
+        <div ref={proRef} className="w-full h-screen flex flex-col justify-around items-center lg:hidden overflow-hidden">
+          <ProgressBarH />
+        </div>
+        <div ref={donateRef} className="w-full h-screen flex flex-col justify-around items-center overflow-hidden">
+          รวมสนับสนุนพวกเราได้ที่
+        </div>
       </div>
-      <div ref={proRef} className="w-full h-screen flex flex-col justify-around items-center lg:hidden overflow-hidden">
-        <ProgressBarH />
-      </div>
-      <div ref={donateRef} className="w-full h-screen flex flex-col justify-around items-center overflow-hidden">
-        รวมสนับสนุนพวกเราได้ที่
-      </div>
-    </div>
+    </>
   );
 };
 
